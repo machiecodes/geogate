9547261f0f69829e472f5ddc42fab84a4b21d37f
MachieCodes <rickytheracc@gmail.com>
2025-03-06 16:16:50 -0600

    Fabric Mod

---
+++ src/main/java/me/ricky/geogate/Geogate.java
@@ -0,0 +1,10 @@
+package me.ricky.geogate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Geogate implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
