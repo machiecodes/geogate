9a807d8a8bd5b321051c9ff8e2ff430fe36d239c
MachieCodes <rickytheracc@gmail.com>
2025-03-07 01:12:43 -0600

    Event Things

--- src/main/java/me/ricky/geogate/Geogate.java
+++ src/main/java/me/ricky/geogate/Geogate.java
@@ -1,10 +1,24 @@
 package me.ricky.geogate;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Geogate implements ModInitializer {
+    public static final Logger LOG = LoggerFactory.getLogger("geogate");
+
     @Override
     public void onInitialize() {
+        ServerLoginConnectionEvents.INIT.register((handler, server) -> {
+            handler.disconnect(Text.of("Bruh"));
+            // Check for vpn and deny them even connecting if so
+        });
 
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            // Once they're actually logged in freeze them in place until they verify
+        });
     }
 }
