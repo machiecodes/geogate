177ddaffa6d8ac0488939a7afd0bd1d1aa3a6c30
MachieCodes <rickytheracc@gmail.com>
2025-03-09 23:20:09 -0500

    Organize Events

--- src/main/java/me/machie/geogate/Geogate.java
+++ src/main/java/me/machie/geogate/Geogate.java
@@ -5,39 +5,49 @@ import me.machie.geogate.config.GeogateConfig;
 import me.machie.geogate.site.WebServer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerLoginNetworkHandler;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Geogate implements ModInitializer {
     public static final Logger LOG = LoggerFactory.getLogger("geogate");
-    public static final GeogateConfig CONFIG = ConfigManager.load();
-
-    public static boolean loadedSafely = false;
+    public static GeogateConfig CONFIG;
 
     @Override
     public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            loadedSafely = WebServer.start();
-            if (!loadedSafely) LOG.error("Failed to start web server, geogate will be inactive!");
-        });
+        ServerLifecycleEvents.SERVER_STARTING.register(this::onServerStarting);
+        ServerLifecycleEvents.SERVER_STOPPING.register(this::onServerStopping);
 
-        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-            WebServer.stop();
-            ConfigManager.save(CONFIG);
-        });
+        ServerLoginConnectionEvents.INIT.register(this::onAttemptedLogin);
+        ServerPlayConnectionEvents.JOIN.register(this::onPlayerJoin);
+    }
 
-        ServerLoginConnectionEvents.INIT.register((handler, server) -> {
-            if (!loadedSafely) return;
+    private void onServerStarting(MinecraftServer server) {
+        CONFIG = ConfigManager.load();
 
-            // Stuff
-        });
+        if (CONFIG == null) {
+            server.stop(false);
+            return;
+        }
 
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            if (!loadedSafely) return;
+        WebServer.start();
+    }
 
-            // Stuff
-        });
+    private void onServerStopping(MinecraftServer server) {
+        WebServer.stop();
+        ConfigManager.save(CONFIG);
+    }
+
+    private void onAttemptedLogin(ServerLoginNetworkHandler handler, MinecraftServer server) {
+        // TODO vpn check
+    }
+
+    private void onPlayerJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
+        // TODO send message and freeze in place
     }
 }
